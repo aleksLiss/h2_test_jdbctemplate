d4001f21863a652a1a58334cd8ee736b8bf92e82
aleksLiss <darklissik.darklissik@gmail.com>
2025-09-27 12:37:37 +0300

    add JdbcUserRepository.java

---
+++ src/main/java/com/aleks/h2/example/repository/JdbcUserRepository.java
@@ -0,0 +1,82 @@
+package com.aleks.h2.example.repository;
+
+import com.aleks.h2.example.mapper.UserMapper;
+import com.aleks.h2.example.model.User;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public class JdbcUserRepository implements UserRepository {
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private UserMapper userMapper;
+    private static final Logger LOGGER = LoggerFactory.getLogger(JdbcUserRepository.class);
+
+    @Override
+    public Optional<User> save(User user) {
+        String sql = "INSERT INTO users(login, password) VALUES(?, ?)";
+        Optional<User> savedUser;
+        try {
+            jdbcTemplate.update(
+                    sql,
+                    user.getLogin(),
+                    user.getPassword()
+            );
+            savedUser = Optional.ofNullable(
+                    jdbcTemplate
+                            .queryForObject(
+                                    "SELECT * FROM users WHERE LOGIN = ?", new Object[]{user.getLogin()}, userMapper
+                            ));
+            LOGGER.info("UserRepository: User was saved");
+        } catch (DuplicateKeyException ex) {
+            LOGGER.warn("UserRepository: This user already exists");
+            throw new RuntimeException("user already exists");
+        }
+        return savedUser;
+    }
+
+    @Override
+    public boolean delete(int id) {
+        String sql = "DELETE FROM users WHERE id = ?";
+        int deleted = 0;
+        try {
+            deleted = jdbcTemplate.update(sql, id);
+            LOGGER.info("UserRepository: User with id: {} was deleted", id);
+        } catch (Exception ex) {
+            LOGGER.warn("UserRepository: User with id: {} was not deleted", id);
+        }
+        return deleted != 0;
+    }
+
+    @Override
+    public Optional<User> getById(int id) {
+        String sql = "SELECT * FROM users WHERE id = ?";
+        User user = null;
+        try {
+            user = jdbcTemplate.queryForObject(sql, new Object[]{id}, userMapper);
+        } catch (Exception ex) {
+            LOGGER.warn("UserRepository: User with id: {} not found", id);
+        }
+        return Optional.ofNullable(user);
+    }
+
+    @Override
+    public Optional<User> getByLogin(String login) {
+        String sql = "SELECT id, login, password FROM users WHERE login = ?";
+        User foundUserByLogin = null;
+        try {
+            foundUserByLogin = jdbcTemplate.queryForObject(sql, new Object[]{login}, userMapper);
+        } catch (Exception ex) {
+            LOGGER.warn("UserRepository: User with login: {} was not found", login);
+        }
+        return Optional.ofNullable(foundUserByLogin);
+    }
+}
