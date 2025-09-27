fbdb496a95c89758fb1466f87b34f98614620ed9
aleksLiss <darklissik.darklissik@gmail.com>
2025-09-27 12:35:06 +0300

    add DatabaseConfig.java

---
+++ src/main/java/com/aleks/h2/example/config/DatabaseConfig.java
@@ -0,0 +1,27 @@
+package com.aleks.h2.example.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DatabaseConfig {
+
+    @Bean
+    public JdbcTemplate jdbcTemplate(DataSource dataSource) {
+        return new JdbcTemplate(dataSource);
+    }
+
+    @Bean
+    public DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .addScript("db/changelog/scripts/schema.sql")
+                .build();
+    }
+}
